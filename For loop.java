68967a386cafed4f2ed35868feece0e6b3aa8533
Nanmaran17 <nanmaran9459@mountzion.ac.in>
2025-08-05 11:43:25 +0530

    Create For loop.java

---
+++ For loop.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class main{
+    public static void main(String[] args){
+        Scanner data = new Scanner(System.in);
+        
+        int i = 10;
+        for(int count = 1; count <= 10; count++)
+        {
+            System.out.println(count);
+        }
+        
+        
+        
+       
+    }
+}
