d1fc41f6a7d7de6b8545a4e90a866c27410bef6d
Nanmaran17 <nanmaran9459@mountzion.ac.in>
2025-08-05 12:05:39 +0530

    Create CountDown.java

---
+++ CountDown.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class main{
+    public static void main(String[] args){
+        Scanner data = new Scanner(System.in);
+        
+        for(int countDown = 10; countDown >= 1; countDown--){
+            
+            System.out.println(countDown);
+            if (countDown == 1){
+            System.out.println("sapadu");
+            break;
+            }
+        }
+        
+        
+        
+       
+    }
+}
