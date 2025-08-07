964575c6309d97a127c8d6fb1d1770e5602351bc
Nanmaran17 <nanmaran9459@mountzion.ac.in>
2025-08-07 10:28:03 +0530

    Create Square root.java

---
+++ Square root.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner data = new Scanner(System.in);
+        System.out.println("Enter the element");
+        int n;
+        n=data.nextInt();
+        System.out.println("Square Root =" + (n*n));
+    }
+    
+}
